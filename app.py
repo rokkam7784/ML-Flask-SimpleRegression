62fdc2b708848192f20f6057dd48c2b14142673f
gymy <abhisonuk123@gmail.com>
2021-06-18 01:18:12 +0530

    added display of salary

--- app.py
+++ app.py
@@ -1,32 +1,36 @@
 import numpy as np
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template,make_response
 import pickle
 
 # creating a flask app
-app = Flask(__name__,template_folder="views/templates",static_folder="views/static",)
+app = Flask(__name__,template_folder="views/templates",static_folder="views/static")
 
 # loading the pickle
 model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/',methods=['GET'])
+
 def welcome():
     if request.method == 'GET':
         return render_template("index.html")
     else:
         return "Bad Request"
 
-@app.route('/',methods=['POST'])
-def predict():
-    if request.method == 'POST':
-        try:
-            yrsOfExperience = float(request.form['yrsOfExperience'])
-            prediction = model.predict([[yrsOfExperience]])
-            result = {"YearsOfExperience": yrsOfExperience,
-                    "Salary": int(prediction[0])}
-            return render_template("index.html")
+@app.route('/predict/<yrsOfExperience>')
+def predict(yrsOfExperience):
+    if request.method == 'GET':        
+        yrsOfExperience = float(yrsOfExperience)
+        prediction = model.predict([[yrsOfExperience]])
+        result = {"YearsOfExperience": yrsOfExperience,
+                "Salary": int(prediction[0])}
+        res = make_response(jsonify(result))
+        res.headers["Access-Control-Allow-Origin"] = "*"
+        res.headers["Access-Control-Allow-Headers"] = "*"
+        res.headers["Access-Control-Allow-Methods"] = "*"
+        return res
     else:
         return"Bad Request"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
--- views/static/main.js
+++ views/static/main.js
@@ -1,4 +1,4 @@
-function validateform() {
+async function validateform() {
     var yrsOfExperience = document.myform.yrsOfExperience.value;
     if (yrsOfExperience == null || yrsOfExperience == "") {
         alert("Years of experience can't be blank");
@@ -8,6 +8,20 @@ function validateform() {
         document.myform.yrsOfExperience.value = '';
         alert("Enter a numerical value as years of experience");
         return false;
+    } else {
+        fetch(`predict/${yrsOfExperience}`, {
+            method: 'GET',
+            redirect: 'follow',
+        }).then(response => response.json())
+        .then(result => {
+          alert(`Your predicted salary would be Rs. ${result['Salary']}/-`);
+          document.myform.yrsOfExperience.value = '';
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+        
     }
+   
 }
 
